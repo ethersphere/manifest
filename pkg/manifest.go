382a9a285581d47a2f15d4600e27f7ae56b95224
mortelli <ortellimarcelo@gmail.com>
2020-07-07 17:44:52 -0300

    manifest: add Entry, Headers, AccessControlParams and CRSparams types

--- pkg/manifest.go
+++ pkg/manifest.go
@@ -3,3 +3,27 @@
 // license that can be found in the LICENSE file.
 
 package manifest
+
+type Entry struct {
+	// temp type
+	reference string // swarm reference to the file
+	// temp type
+	fileInfo     string  // file or dir info akin to one given on file system
+	headers      Headers // HTTP response headers, e.g. content type
+	accessParams AccessControlParams
+	crsParams    CRSparams // info needed for erasure coding
+}
+
+// BoS says this should be:
+// define type headers
+//     `content.type  [segment size]byte`
+// how can this be reconciled?
+type Headers map[string]string
+
+// unkown for now
+type AccessControlParams struct {
+}
+
+// unknown for now
+type CRSparams struct {
+}
