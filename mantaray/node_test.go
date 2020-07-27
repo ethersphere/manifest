75299174af30c57d7331fddf9d2ddebabbb6b9ed
Nemanja Zbiljić <nemanja.zbiljic@gmail.com>
2020-07-27 13:52:47 +0200

    Add Remove function for node

--- mantaray/node_test.go
+++ mantaray/node_test.go
@@ -6,6 +6,7 @@ package mantaray
 
 import (
 	"bytes"
+	"errors"
 	"testing"
 )
 
@@ -46,5 +47,74 @@ func TestAddAndLookup(t *testing.T) {
 				t.Fatalf("expected value %x, got %x", d, m)
 			}
 		}
+	}
+}
+
+func TestRemove(t *testing.T) {
+	for _, tc := range []struct {
+		name     string
+		toAdd    [][]byte
+		toRemove [][]byte
+	}{
+		{
+			name: "simple",
+			toAdd: [][]byte{
+				[]byte("index.html"),
+				[]byte("img/1.png"),
+				[]byte("img/2.png"),
+				[]byte("robots.txt"),
+			},
+			toRemove: [][]byte{
+				[]byte("img/2.png"),
+			},
+		},
+		{
+			name: "nested-prefix-is-not-collapsed",
+			toAdd: [][]byte{
+				[]byte("index.html"),
+				[]byte("img/1.png"),
+				[]byte("img/2/test1.png"),
+				[]byte("img/2/test2.png"),
+				[]byte("robots.txt"),
+			},
+			toRemove: [][]byte{
+				[]byte("img/2/test1.png"),
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			n := New()
+
+			for i := 0; i < len(tc.toAdd); i++ {
+				c := tc.toAdd[i]
+				err := n.Add(c, c, nil)
+				if err != nil {
+					t.Fatalf("expected no error, got %v", err)
+				}
+				for j := 0; j < i; j++ {
+					d := tc.toAdd[j]
+					m, err := n.Lookup(d, nil)
+					if err != nil {
+						t.Fatalf("expected no error, got %v", err)
+					}
+					if !bytes.Equal(m, d) {
+						t.Fatalf("expected value %x, got %x", d, m)
+					}
+				}
+			}
+
+			for i := 0; i < len(tc.toRemove); i++ {
+				c := tc.toRemove[i]
+				err := n.Remove(c, nil)
+				if err != nil {
+					t.Fatalf("expected no error, got %v", err)
+				}
+				_, err = n.Lookup(c, nil)
+				if !errors.Is(err, ErrNotFound) {
+					t.Fatalf("expected not found error, got %v", err)
+				}
+			}
+
+		})
 	}
 }
